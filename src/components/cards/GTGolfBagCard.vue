af96956382798e66acbb88ed374e52867ab156b1
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-11 08:13:09 -0400

    build GolfBagCard component, update icon styling on nav and sidebars

---
+++ src/components/cards/GTGolfBagCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  golfbag: {}
+}>()
+</script>
+
+<template>
+  <div class="border rounded-md border-black shadow hover:shadow-lg">
+    <div class="flex flex-row p-2">
+      <img src="../icons/GTImageNotFound.svg" alt="Image not found" class="h-20 w-20 mr-2" />
+      <div>
+        <p>{{ props.golfbag.make }}</p>
+        <p>{{ props.golfbag.model }}</p>
+        <p>{{ props.golfbag.nickname }}</p>
+      </div>
+    </div>
+  </div>
+</template>
