89bf5acb23a68c1a95dc13c51c78147f6d41ae0a
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-12 15:39:16 -0400

    complete useAxios composable

--- src/views/GolfBagsView.vue
+++ src/views/GolfBagsView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import { useAxios } from '@/composables/useAxios'
 import axios from 'axios'
 import AddGolfBagForm from '../forms/AddGolfBagForm.vue'
@@ -8,24 +7,13 @@ import GTGolfBagCard from '../components/cards/GTGolfBagCard.vue'
 axios.defaults.withCredentials = true
 axios.defaults.withXSRFToken = true
 
-const golfbags = ref([])
-
-//TODO: Extract 'naked' axios request to a composable.
-const fetchGolfBags = () => {
-  const { requestData } = useAxios('GET', 'golfbags')
-  console.log('destructured data', requestData.value)
-  golfbags.value = requestData.value
-}
-
-onMounted(() => {
-  fetchGolfBags()
-})
+const { requestData: golfbags } = useAxios('GET', 'golfbags')
 </script>
 
 <template>
   <div class="flex justify-between">
-    <div class="space-y-4">
-      <ul v-for="golfbag in golfbags">
+    <div v-if="golfbags" class="space-y-4">
+      <ul :key="golfbag.id" v-for="golfbag in golfbags">
         <GTGolfBagCard :golfbag></GTGolfBagCard>
       </ul>
     </div>
