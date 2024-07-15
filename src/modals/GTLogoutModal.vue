6a722c4eca82c1c723daff58002a061ea57f590b
bbowler1740 <bbowler1740@conestogac.on.ca>
2024-07-15 07:30:36 -0400

    style logout modal

--- src/modals/GTLogoutModal.vue
+++ src/modals/GTLogoutModal.vue
@@ -13,11 +13,13 @@ const open = ref(false)
   <Teleport to="body">
     <div
       v-if="open"
-      class="fixed left-1/2 top-1/2 z-999 border w-60 h-24 border-black rounded-md bg-secondary"
+      class="fixed left-1/2 top-1/2 z-999 -ml-28 border w-70 h-24 border-black rounded-md bg-secondary p-2 space-y-4"
     >
       <p>Are you sure you'd like to logout?</p>
-      <GTButton @click="auth.logout()" class="hover:text-white">Yes, Logout</GTButton>
-      <GTButton @click="open = false" class="hover:text-white">No, Cancel</GTButton>
+      <div class="flex flex-row justify-around">
+        <GTButton @click="auth.logout()" class="hover:text-white">Logout</GTButton>
+        <GTButton @click="open = false" class="hover:text-white">Cancel</GTButton>
+      </div>
     </div>
   </Teleport>
 </template>
