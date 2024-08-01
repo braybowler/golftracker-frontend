d3db3076c4ba5b65db815331df75930ef39b3346
Brayden Bowler <braybowler1995@gmail.com>
2024-08-01 15:32:08 -0400

    lint, format, type-check

---
+++ src/forms/AddBallForm.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref, unref } from 'vue'
+import { useAxios } from '@/composables/useAxios'
+import GTNavButton from '@/components/GTNavButton.vue'
+
+const make = ref('')
+const model = ref('')
+const nickname = ref('')
+
+const createGolfBag = () => {
+  useAxios('POST', 'golfbags', {
+    make: unref(make),
+    model: unref(model),
+    nickname: unref(nickname)
+  })
+}
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <p>Want to add another bag? Enter the details here:</p>
+    <div class="bg-secondary border border-black rounded-md flex flex-col p-4 gap-2 w-full">
+      <div class="flex flex-row justify-between">
+        <label for="email">Make</label>
+        <input
+          type="text"
+          v-model="make"
+          placeholder="Please enter the make of your bag"
+          class="border border-black rounded-md"
+        />
+      </div>
+      <div class="flex flex-row justify-between">
+        <label for="password">Model</label>
+        <input
+          type="text"
+          v-model="model"
+          placeholder="Please enter the model of your bag"
+          class="border border-black rounded-md"
+        />
+      </div>
+      <div class="flex flex-row justify-between">
+        <label for="password">Nickname</label>
+        <input
+          type="text"
+          v-model="nickname"
+          placeholder="Please enter a nickname for your bag"
+          class="border border-black rounded-md"
+        />
+      </div>
+      <div class="flex flex-col justify-center">
+        <GTNavButton
+          @click="createGolfBag()"
+          url="/golfbags"
+          class="border border-black rounded-md text-center"
+          >Create Golf Bag</GTNavButton
+        >
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/GolfClubs/CreateClubsView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AddClubForm from '@/forms/AddClubForm.vue'
+import GTNavButton from '@/components/GTNavButton.vue'
+</script>
+
+<template>
+  <div>
+    <GTNavButton url="/golfclubs" class="border border-black rounded-md">Back to Clubs</GTNavButton>
+    <div class="flex justify-center">
+      <AddClubsForm class="w-1/3"></AddClubsForm>
+    </div>
+  </div>
+</template>
