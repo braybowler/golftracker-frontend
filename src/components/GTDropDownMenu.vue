5be80034ffa0a5d5b574ac9f795470b2694103ba
Brayden Bowler <braybowler1995@gmail.com>
2024-08-20 10:04:35 -0400

    style dropdown menu

--- src/components/GTDropDownMenu.vue
+++ src/components/GTDropDownMenu.vue
@@ -30,26 +30,32 @@ const toggleIsExpanded = () => {
       />
     </svg>
   </button>
-  <div v-if="isHamburgerMenuExpanded">
-    <div class="flex flex-col">
-      <GTNavLink :url="'/profile'" class="flex flex-row hover:text-white">
-        Profile
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+  <div
+    class="flex flex-col bg-primary w-36 fixed top-10 right-0 p-2 space-y-1"
+    v-if="isHamburgerMenuExpanded"
+  >
+      <GTNavLink :url="'/profile'" class="flex flex-row justify-end gap-2 hover:text-white">
+        <div>
+          Profile
+        </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+        </div>
       </GTNavLink>
-      <GTNavLink :url="'/settings'" class="flex flex-row hover:text-white">
+      <GTNavLink :url="'/settings'" class="flex flex-row justify-end gap-2 hover:text-white">
         Settings
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -71,7 +77,11 @@ const toggleIsExpanded = () => {
           />
         </svg>
       </GTNavLink>
-      <GTLogoutModal></GTLogoutModal>
+      <div class="flex flex-row justify-end gap-2 hover:text-white">
+        <GTLogoutModal></GTLogoutModal>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+        </svg>
+      </div>
     </div>
-  </div>
 </template>
