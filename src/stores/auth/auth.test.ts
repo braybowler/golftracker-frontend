5e0d89553ab468829a0f4806098e4f276afb4f9a
Brayden Bowler <braybowler1995@gmail.com>
2024-09-17 21:39:27 -0400

    test register method

--- src/stores/auth/auth.test.ts
+++ src/stores/auth/auth.test.ts
@@ -1,25 +1,16 @@
-import { describe, test, vi, expect } from 'vitest'
-import { useAuth } from '@/stores/auth/auth'
-import router from '@/router'
+import { describe, test, vi, expect, beforeEach, type Mock } from 'vitest'
+import {useAuth} from '@/stores/auth/auth'
+import axios from 'axios'
+import { createPinia, setActivePinia } from 'pinia'
+import { useRouter } from 'vue-router'
 
+const csrfUrl = import.meta.env.VITE_CSRF_URL
 const baseUrl = import.meta.env.VITE_API_BASE_URL
 
-vi.mock('@/stores/auth/auth', async () => ({
-  useAuth: vi.fn(() => ({
-    register: vi.fn(),
-    login: vi.fn(),
-    logout: vi.fn()
-  }))
-}))
-
-// vi.mock('@/router', async (importOriginal) => {
-//   const original = await importOriginal() // type is inferred
-//   return {
-//     ...original,
-//     // replace some exports
-//     replace: vi.fn(),
-//   }
-// })
+vi.mock('axios')
+vi.mock('@/router', () => ({
+  useRouter: vi.fn()
+}));
 
 const guest = {
   name: 'Test User',
@@ -27,46 +18,66 @@ const guest = {
   password: 'password'
 }
 
-describe('Auth.ts', () => {
+describe('useAuth.ts', () => {
+
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
 
   describe('Register', () => {
-    test('Registration requests are submitted to the registration endpoint', () => {
-      const registrationEndpoint = baseUrl + 'register'
-      const authStore = useAuth()
-      authStore.register(guest.name, guest.email, guest.password);
+    const registrationEndpoint = baseUrl + 'register';
 
-      expect(authStore.register).toHaveBeenCalledWith(guest.name, guest.email, guest.password)
-      expect(authStore.register).toHaveBeenCalledOnce()
+    test('Registration requests make an initial request to the csrf endpoint', async () => {
+      (axios.get as Mock).mockResolvedValueOnce({});
+
+      const { register } = useAuth()
+      await register(guest.name, guest.email, guest.password)
+
+      expect(axios.get).toHaveBeenCalledWith(csrfUrl)
     })
 
-    test('A successful registration request routes a user to the dashboard', () => {
-      const authStore = useAuth()
-      authStore.register(guest.name, guest.email, guest.password);
+    test('Registration requests are submitted to the registration endpoint', async () => {
+      (axios.get as Mock).mockResolvedValueOnce({});
+      const mockResponse = {
+          user: {
+            id: 1,
+            name: 'test user',
+            email: 'test@example.com',
+            email_verified_at: null,
+            last_active_at: null,
+            created_at: '2024-09-08T17:28:16.000000Z',
+            updated_at: '2024-09-08T17:28:16.000000Z'
+          }
+        };
+      (axios.post as Mock).mockResolvedValueOnce(mockResponse);
 
-      expect(router.replace).toHaveBeenCalledWith({name: 'Dashboard'})
-      expect(router.currentRoute.value.name).toBe('Dashboard')
+      const { register } = useAuth()
+      await register(guest.name, guest.email, guest.password)
+
+      expect(axios.post).toHaveBeenCalledWith(
+        registrationEndpoint,
+        { name: guest.name, email: guest.email, password: guest.password }
+      )
     })
+
+    test.todo('A successful registration request routes a user to the dashboard', async () => {
+
+    })
+
   })
 
   describe('Login', () => {
-    test('Login requests are submitted to the login endpoint', () => {
-      const loginEndpoint = baseUrl + 'login'
 
-      const authStore = useAuth()
-      authStore.login(guest.email, guest.password);
-
-      expect(authStore.login).toHaveBeenCalledWith(guest.email, guest.password)
+    test.todo('Login requests are submitted to the login endpoint', () => {
     })
+
   })
 
   describe('Logout', () => {
-    test('Logout requests are submitted to the logout endpoint', () => {
-      const logoutEndpoint = baseUrl + 'logout'
 
-      const authStore = useAuth()
-      authStore.logout();
-
-      expect(authStore.logout).toHaveBeenCalled()
+    test.todo('Logout requests are submitted to the logout endpoint', () => {
     })
+
   })
+
 })
