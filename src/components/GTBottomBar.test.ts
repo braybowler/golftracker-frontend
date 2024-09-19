ee59d80139950b0ed7a6362aa25e74e94dbc1f62
Brayden Bowler <braybowler1995@gmail.com>
2024-09-19 09:12:23 -0400

    initial tests for GTBottomBar component

---
+++ src/components/GTBottomBar.test.ts
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, type Mock, test, vi } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import GTBottomBar from '@/components/GTBottomBar.vue'
+import { createPinia, setActivePinia } from 'pinia'
+
+vi.mock('@/stores/auth/auth', async () => {
+  const actual = await vi.importActual('@/stores/auth/auth')
+  const mockIsAuthed = vi.fn(() => { return true })
+
+  return {
+    ...actual,
+    useAuth: ()=> {
+      return {isAuthed: mockIsAuthed}
+    }
+  }
+})
+
+describe('GTBottomBar', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  test('Component can render', () => {
+    const wrapper = shallowMount(GTBottomBar)
+
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  test('Component does render when there is an authorized user', () => {
+    const wrapper = shallowMount(GTBottomBar)
+
+    expect(wrapper.html()).toContain('<nav class="bg-primary w-screen">')
+  })
+
+  test.todo('Component does not render when there is not an authorized user', () => {
+    const wrapper = shallowMount(GTBottomBar)
+
+    expect(wrapper.html()).toContain('')
+  })
+})
