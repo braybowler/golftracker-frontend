4837924879d551c6f518cfc292ad3a8d9e50ff4c
Brayden Bowler <braybowler1995@gmail.com>
2024-09-26 17:21:00 -0400

    update navbartest

--- src/components/GTNavBar.test.ts
+++ src/components/GTNavBar.test.ts
@@ -1,39 +1,58 @@
-import { beforeEach, describe, expect, test, vi } from 'vitest'
-import { createPinia, setActivePinia } from 'pinia'
-import { shallowMount } from '@vue/test-utils'
+import { describe, expect, test, vi } from 'vitest'
+import { mount, shallowMount } from '@vue/test-utils'
 import GTNavBar from '@/components/GTNavBar.vue'
+import { createTestingPinia } from '@pinia/testing'
+import { useAuth } from '@/stores/auth/auth'
 
-vi.mock('@/stores/auth/auth', async () => {
-  const actual = await vi.importActual('@/stores/auth/auth')
-  const mockIsAuthed = vi.fn()
-
-  return {
-    ...actual,
-    useAuth: ()=> {
-      return {isAuthed: mockIsAuthed}
-    }
-  }
-})
 
 describe('GTNavBar', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-  })
 
   test('Component can render', () => {
-    const wrapper = shallowMount(GTNavBar)
+    const wrapper = shallowMount(GTNavBar, {
+      global: {
+        plugins: [createTestingPinia({
+          createSpy: vi.fn,
+          initialState: {
+            auth: {
+              isAuthed: true
+            }
+          }
+        })]
+      }
+    })
 
     expect(wrapper.exists()).toBe(true)
   })
 
   test('Component renders the guest view if the user is not authenticated', () => {
+    const pinia = createTestingPinia({
+      createSpy: vi.fn
+    })
+    const auth = useAuth(pinia)
+    auth.isAuthed = false
     const wrapper = shallowMount(GTNavBar)
 
     expect(wrapper.html()).toContain('<g-t-nav-link-stub url="/" class="hover:text-white"></g-t-nav-link-stub>')
   })
 
   test('Component renders the user view if the user is authenticated', async () => {
-    const wrapper = shallowMount(GTNavBar)
+    const wrapper = shallowMount(GTNavBar, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              auth: { isAuthed: false }
+            },
+            createSpy: vi.fn
+          })
+        ]
+      }
+    })
+
+    const auth = useAuth()
+    console.log(auth.isAuthed)
+    auth.isAuthed = true
+    console.log(auth.isAuthed)
 
     expect(wrapper.html()).toContain('<g-t-drop-down-menu-stub></g-t-drop-down-menu-stub>')
   })
