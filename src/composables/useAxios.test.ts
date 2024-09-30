d2ba163c5062a2b43912bf9505334f72c9058b94
Brayden Bowler <braybowler1995@gmail.com>
2024-09-30 08:38:07 -0400

    Refactor useAxios to be encapsulated into a function - Write tests for changing functionality

--- src/composables/useAxios.test.ts
+++ src/composables/useAxios.test.ts
@@ -1,70 +1,99 @@
-import { ref } from 'vue'
+import { afterEach, beforeEach, describe, expect, type Mock, test, vi } from 'vitest'
+import { useAxios } from '@/composables/useAxios'
 import axios from 'axios'
-
-axios.defaults.withCredentials = true
-axios.defaults.withXSRFToken = true
+import { setActivePinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
+import { throws } from 'assert'
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL
 
-export async function useAxios(method: string, requestUrl: string, body?: {}) {
-  const requestData = ref(null)
-  const requestError = ref(null)
-  const fullUrl = baseUrl + requestUrl
+vi.mock('axios')
+describe('useAxios.ts', () => {
+  beforeEach(() => {
+    setActivePinia(
+      createTestingPinia({
+        createSpy: vi.fn,
+        stubActions: false
+      }),
+    )
+  })
 
-  switch (method) {
-    case 'DELETE':
-      axios
-        .delete(fullUrl)
-        .then(function(response) {
-          console.log(response)
-          requestData.value = response.data.data ? response.data.data : response.data
-        })
-        .catch(function(error) {
-          console.log('DELETE error: ', error)
-          requestError.value = error
-        })
-      break
-    case 'GET':
-      try {
-        const response = await axios.get(fullUrl)
-        requestData.value = response.data.data ? response.data.data : response.data
-      } catch (e) {
-        console.log('GET error: ', e)
-        requestError.value = e
-      }
-      break
-    case 'POST':
-      axios
-        .post(fullUrl, body)
-        .then(function(response) {
-          console.log(response)
-          requestData.value = response.data.data
-        })
-        .catch(function(error) {
-          console.log('POST error: ', error)
-          requestError.value = error
-        })
-      break
-    //Only invoked by the useAuth store in src/stores/auth/auth.ts
-    case 'LOGOUT':
-      axios
-        .post(fullUrl, body)
-        .then(function(response) {
-          if (response.status === 204) {
-            console.log(response)
-            requestData.value = response.data.data ? response.data.data : response.data
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  const testEndpoint = baseUrl + 'test'
+
+  describe('requestMethodSelector', () => {
+
+    test('method routes calls with GET parameter to correct HTTP request type', async () => {
+      const {requestMethodSelector} = useAxios()
+// todo: fix this test with proper setup
+      await requestMethodSelector("GET", 'test', {})
+
+      expect(axios.get).toHaveBeenCalledWith(testEndpoint)
+      expect(axios.post).not.toHaveBeenCalled()
+      expect(axios.delete).not.toHaveBeenCalled()
+    })
+
+    test('method routes calls with POST parameter to correct HTTP request type', async () => {
+      ;(axios.post as Mock).mockReturnValue({
+        data: {
+          data: {
+            name: 'test'
+            }
           }
-        })
-        .catch(function(error) {
-          console.log('LOGOUT error: ', error)
-          requestError.value = error
-        })
-      break
-  }
+        }
+      )
+
+      const {requestMethodSelector} = useAxios()
+
+      await requestMethodSelector("POST", 'test', {})
+
+      expect(axios.post).toHaveBeenCalledWith(testEndpoint, {})
+      expect(axios.get).not.toHaveBeenCalled()
+      expect(axios.delete).not.toHaveBeenCalled()
+    })
+
+    test('method routes calls with DELETE parameter to correct HTTP request type', async () => {
+      const {requestMethodSelector} = useAxios()
+// todo: fix this test with proper setup
+      await requestMethodSelector("DELETE", 'test')
+
+      expect(axios.delete).toHaveBeenCalledWith(testEndpoint)
+      expect(axios.post).not.toHaveBeenCalled()
+      expect(axios.get).not.toHaveBeenCalled()
+    })
+
+    test.each([
+      { method: 'GET', functionCall: axios.get as Mock, expectedParams: [testEndpoint]},
+      { method: 'POST', functionCall: axios.post as Mock, expectedParams: [testEndpoint, {}]},
+      { method: 'DELETE', functionCall: axios.delete as Mock, expectedParams: [testEndpoint]}
+    ])
+    ('it will catch and re-throw error from request $method', async ({ method, functionCall, expectedParams }) => {
+      const spy = vi.spyOn(console, 'error').mockImplementation(() => ({}))
+      let thrownError = undefined
+      functionCall.mockRejectedValue({
+        status: 404
+      })
+
+      const {requestMethodSelector} = useAxios()
+
+      try {
+        await requestMethodSelector(method, 'test', {})
+      } catch (e) {
+        thrownError = e
+      }
 
 
-  return {
-    requestData,
-    requestError
-  }
-}
+      expect(spy).toHaveBeenCalled()
+      expect(functionCall).toHaveBeenCalledWith(...expectedParams)
+      expect(thrownError).not.toBeUndefined()
+
+      spy.mockReset()
+    })
+
+    test.todo('it makes a CSRF request if the XSRF_TOKEN is not present')
+    test.todo('it does not make a CSRF request if the XSRF_TOKEN is present')
+  })
+})
--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -5,70 +5,48 @@ axios.defaults.withCredentials = true
 axios.defaults.withXSRFToken = true
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL
+const csrfUrl = import.meta.env.VITE_CSRF_URL
 
-export function useAxios(method: string, requestUrl: string, body?: {}) {
-  const requestData = ref(null)
-  const requestError = ref(null)
-  const fullUrl = baseUrl + requestUrl
+export function useAxios() {
+  // const requestData = ref(null)
+  // const requestError = ref(null)
+  // const fullUrl = baseUrl + requestUrl
 
-  switch (method) {
-    case 'DELETE':
-      axios
-        .delete(fullUrl)
-        .then(function (response) {
-          console.log(response)
-          requestData.value = response.data.data ? response.data.data : response.data
-        })
-        .catch(function (error) {
-          console.log('DELETE error: ', error)
-          requestError.value = error
-        })
-      break
-    case 'GET':
-      axios
-        .get(fullUrl)
-        .then(function (response) {
-          console.log(response)
-          //This is here to support a single resource being returned (i.e. a request to a show action),
-          //since Laravel paginate() forces an additional 'data' wrapping.
-          requestData.value = response.data.data ? response.data.data : response.data
-        })
-        .catch(function (error) {
-          console.log('GET error: ', error)
-          requestError.value = error
-        })
-      break
-    case 'POST':
-      axios
-        .post(fullUrl, body)
-        .then(function (response) {
-          console.log(response)
-          requestData.value = response.data.data
-        })
-        .catch(function (error) {
-          console.log('POST error: ', error)
-          requestError.value = error
-        })
-      break
-    //Only invoked by the useAuth store in src/stores/auth/auth.ts
-    case 'LOGOUT':
-      axios
-        .post(fullUrl, body)
-        .then(function (response) {
-          if (response.status === 204) {
-            console.log(response)
-            requestData.value = response.data.data ? response.data.data : response.data
-          }
-        })
-        .catch(function (error) {
-          console.log('LOGOUT error: ', error)
-          requestError.value = error
-        })
-      break
+  async function requestMethodSelector(method: string, requestUrl: string, body?: {}) {
+    const fullUrl = baseUrl + requestUrl;
+
+    if (!checkCsrfCookie()) {
+      await axios.get(csrfUrl)
+    }
+
+    let response = undefined;
+
+    try {
+        switch (method) {
+          case 'DELETE':
+              response = await axios.delete(fullUrl)
+            break
+          case 'GET':
+              response = await axios.get(fullUrl)
+            break
+          case 'POST':
+              response = await axios.post(fullUrl, body)
+            break
+        }
+
+      return response?.data.data ? response.data.data : response.data
+    } catch (e) {
+      console.error('Error with POST request: ', e)
+      throw e
+    }
+  }
+
+  function checkCsrfCookie() {
+      console.log('checking cookie')
+     return document.cookie.indexOf('XSRF_TOKEN')
   }
 
   return {
-    requestData,
-    requestError
+    requestMethodSelector
   }
 }
